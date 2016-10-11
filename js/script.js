1e9f39c6296ae938d739ac54e554cd15fbf59b4e
Christopher Brown <cbrown119955@gmail.com>
2016-10-11 14:46:16 -0700

    Touch up style, add display for size

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 /*The size of a row or a column on our board in terms of blocks*/
-var numBlocks = 30;
+var numBlocks = 12;
+
+/*Minimum and maximum allowed number of blocks*/
+var minBlocks = 1;
+var maxBlocks = 38;
 
 /*The size of a row or column on our board in terms of pixels*/
 var pxSize = 600;
 
 
 $(document).ready(function() {
-  renewGrid(numBlocks);
+    renewGrid(numBlocks);
 
     $('#board').on('mouseenter', '.block', function() {
         $(this).removeClass('highlighted');
@@ -19,21 +23,29 @@ $(document).ready(function() {
 
 });
 
-function renewGrid(size){
-  $('#board *').remove();
+function renewGrid(size) {
+    $('#board *').remove();
 
-  /*Calculate the proper height and width (or dimensions) for
-   each block in order to maintain the proper grid size*/
-   var dim = pxSize / numBlocks;
+    /*Calculate the proper height and width (or dimensions) for
+     each block in order to maintain the proper grid size*/
+    var dim = pxSize / numBlocks;
 
-  for (var i = 0; i < (size * size); i++) {
-      $('#board').append('<div class="block" style="width: '+dim+'px;height: '+dim+'px;"></div>');
-      if (i !== 0 && (i + 1) % size === 0) {
-          $('#board').append('<br>');
-      }
-  }
+    for (var i = 0; i < (size * size); i++) {
+        $('#board').append('<div class="block" style="width: ' + dim + 'px;height: ' + dim + 'px;"></div>');
+        if (i !== 0 && (i + 1) % size === 0) {
+            $('#board').append('<br>');
+        }
+    }
+
+    $('#footer > p').text('Current size: ' + numBlocks);
 }
 
-$('#clear').on('click', function(){
-  renewGrid(numBlocks);
+$('#clear').on('click', function() {
+    var input = +$('input').val();
+    if (input >= minBlocks && input <= maxBlocks)
+        numBlocks = +$('input').val();
+
+    $('input').val('');
+
+    renewGrid(numBlocks);
 });
